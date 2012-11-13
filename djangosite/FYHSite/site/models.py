5f18c09d8202fc30e68214876e8cf23cd18bd466
Matt Stender <mgstender1@catamount.wcu.edu>
2012-11-13 16:49:58 -0500

    Aded sql tables and entities translated to models

--- djangosite/FYHSite/site/models.py
+++ djangosite/FYHSite/site/models.py
@@ -1,15 +1,61 @@
 from django.db import models
 class Products(models.Model):
+    Product_ID = models.IntegerField()
     UPCCode = models.IntegerField()
-    ProductBrand = models.CharField(max_length = 20)
-    ProductType = models.CharField(max_length = 10)
     ProductName = models.CharField(max_length = 100)
+    ProductSize = models.CharField(max_length = 10)
+    ProductWeight = models.CharField(max_lenght = 10)
     ProductDescription = models.TextField()
-    ProductSellPrice = models.FloatField(default = 10.00)
-    ProductWholesalePrice = models.FloatField(default = 3.00)
-    AmountInStock = models.IntegerField(default = 25)
-    Department = models.CharField(max_length = 10)
-    InStock = models.BooleanField(default = 1)
     
     def __unicode__(self):
         return self.ProductName
+
+class Brand(models.Model):
+    Brand_ID = models.IntegerField()
+    BrandName = models.CharField(max_length = 100)
+
+    def __unicode__(self):
+        return self.BrandName
+
+class Vender(models.Model):
+    Vendor_ID = models.IntegerField()
+    VendorName = models.CharField(max_length = 100)
+
+    def __unicode__(self):
+        return self.VendorName
+
+class Store(models.Model):
+    Store_ID = models.IntegerField()
+    StoreName = models.CharField(max_length = 100)
+    Latitude = models.CharField(max_length = 10)
+    Longitude = models.CharField(max_length = 10)
+    HoursOpen = models.CharField(max_length 10)
+    DaysOpen = models.CharField(max_length 8)
+
+    def __unicode__(self):
+        return self.StoreName
+
+class Address(models.Model):
+    AddressLineOne = models.CharField(max_length = 100)
+    AddressLineTwo = models.CharField(max_length = 100)
+    City = models.CharField(max_length = 50)
+    State = models.CharField(max_length = 50)
+    ZIPCode = models.CharField(max_length = 5)
+
+    def __unicode__(self):
+        return self.AddressLineOne + self.AddressLineTwo + City + State + ZIPCode
+
+class Customer(models.Model):
+    Customer_ID = models.IntegerField()
+    FrequentShopperNumber = models.IntegerField()
+    FirstName = models.CharField(max_length = 50)
+    LastName = models.CharField(max_length = 50)
+    EmailAddress = models.CharField(max_length = 100)
+    PointsGained = models.IntegerField()
+    Username = models.CharField(max_length = 20)
+    Password = models.CharField(max_length = 20)
+    Salt
+
+    def __unicode__(self):
+        return self.FirstName + self.LastName
+
--- sql/FYESetup.sql
+++ sql/FYESetup.sql
@@ -7,20 +7,10 @@ CREATE DATABASE IF NOT EXISTS cs453db2;
 #create tables
 CREATE TABLE `cs453db2`.`Customers`
 (
-  Id int NOT NULL AUTO_INCREMENT,
+  Customer_Id int NOT NULL AUTO_INCREMENT,
   FrequentShopperNumber int,
   LastName varchar(14),
   FirstName varchar(14),
-  ShippingAddress varchar(50),
-  ShippingAddress2 varchar(50),
-  ShippingCity varchar(18),
-  ShippingState char(2),
-  ShippingZIPCode varchar(9),
-  BillingAddress varchar(50),
-  BillingAddress2 varchar(50),
-  BillingCity varchar(18),
-  BillingState char(2),
-  BillingZIPCode varchar(9),
   EmailAddress varchar(30),
   PointsGained int,
   Username varchar(18),
@@ -40,12 +30,8 @@ CREATE TABLE `cs453db2`.`ShopsAt`
 
 CREATE TABLE `cs453db2`.`Stores`
 (
-  Id int NOT NULL AUTO_INCREMENT,
+  Store_Id int NOT NULL AUTO_INCREMENT,
   StoreName varchar(18),
-  StoreAddress varchar(18),
-  StoreCity varchar(18),
-  StoreState char(2),
-  StoreZIPCode varchar(18),
   Latitude varchar(18),
   Longitude varchar(18),
   HoursOpen TEXT,
@@ -64,36 +50,28 @@ CREATE TABLE `cs453db2`.`SellsTo`
 
 CREATE TABLE `cs453db2`.`Vendors`
 (
-  Id int NOT NULL AUTO_INCREMENT,
+  Vendor_Id int NOT NULL AUTO_INCREMENT,
   VendorName varchar(18),
-  ProductBrand varchar(18),
   PRIMARY KEY (Id),
   FOREIGN KEY (ProductBrand) REFERENCES Brands (ProductBrand)
 );
 
 CREATE TABLE `cs453db2`.`Brands`
 (
-  Id int NOT NULL AUTO_INCREMENT,
-  ProductBrand varchar(18),
-  ProductsSold varchar(18),
-  ProductSizes varchar(18),
-  ProductWeight varchar(18),
+  Brand_Id int NOT NULL AUTO_INCREMENT,
+  BrandName varchar(30),
   PRIMARY KEY (Id)
 );
 
 CREATE TABLE `cs453db2`.`Products`
 (
-  Id int NOT NULL AUTO_INCREMENT,
+  Product_Id int NOT NULL AUTO_INCREMENT,
   UPCCode int,
-  ProductBrand varchar(18) REFERENCES Brands (ProductBrand),
-  ProductSize varchar(18),
   ProductName varchar(18),
+  ProductSize varchar(18),
+  ProductWeight varchar(18),
   ProductDescription TEXT,
-  Department varchar(18),
   PRIMARY KEY (Id),
-  FOREIGN KEY (ProductBrand) REFERENCES Brands (ProductBrand),
-  FOREIGN KEY (ProductType) REFERENCES ProductTypes (Category),
-  FOREIGN KEY (Department) REFERENCES ProductTypes (Department)
 );
 
 CREATE TABLE `cs453db2`.`ProductTypes`
