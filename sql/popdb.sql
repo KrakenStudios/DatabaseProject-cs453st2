b73fbf8dd56f42749e65a3b1de199dfacf6c12ca
Chris Ward <cward@polaris.cs.wcu.edu>
2012-11-14 02:40:03 -0500

    Added sql that populates products with data - revised for new models. Fixed login page so register link works. Changed activation email text to go to polaris site.

---
+++ sql/popdb.sql
@@ -0,0 +1,20 @@
+#music
+INSERT INTO `cs453db2`.`site_products` (`Product_ID`, `UPCCode`, `ProductName`, `ProductSize`, `ProductWeight`,`ProductDescription`) VALUES (1, 98324, 'Men In Black Soundtrack', 'small', '12g','The soundtrack from the hit movie!');
+INSERT INTO `cs453db2`.`site_products` (`Product_ID`, `UPCCode`, `ProductName`, `ProductSize`, `ProductWeight`,`ProductDescription`) VALUES (2, 98325, 'Now Thats what I call Music Vol 993', 'small', '12g','Surprise! They still make these.');
+INSERT INTO `cs453db2`.`site_products` (`Product_ID`, `UPCCode`, `ProductName`, `ProductSize`, `ProductWeight`,`ProductDescription`) VALUES (3, 98326, 'Carter IV', 'small', '12g','President Carter is back with a new hit album');
+INSERT INTO `cs453db2`.`site_products` (`Product_ID`, `UPCCode`, `ProductName`, `ProductSize`, `ProductWeight`,`ProductDescription`) VALUES (4, 98327, 'Americana', 'small', '12g','The best Offspring album, ever.');
+INSERT INTO `cs453db2`.`site_products` (`Product_ID`, `UPCCode`, `ProductName`, `ProductSize`, `ProductWeight`,`ProductDescription`) VALUES (5, 98328, 'Yoll Rebel to Anything', 'small', '12g','Featuring Shut Me up and Straight to Video!');
+
+#movies
+INSERT INTO `cs453db2`.`site_products` (`Product_ID`, `UPCCode`, `ProductName`, `ProductSize`, `ProductWeight`,`ProductDescription`) VALUES (6, 98329, 'Men In Black', 'small', '12g','The movie from the hit soundtrack!');
+INSERT INTO `cs453db2`.`site_products` (`Product_ID`, `UPCCode`, `ProductName`, `ProductSize`, `ProductWeight`,`ProductDescription`) VALUES (7, 98330, 'Ghostbusters', 'small', '12g','Busting ectoplasim one ghost at a time');
+INSERT INTO `cs453db2`.`site_products` (`Product_ID`, `UPCCode`, `ProductName`, `ProductSize`, `ProductWeight`,`ProductDescription`) VALUES (8, 98331, 'Terminator', 'small', '12g','The top apocalyptic hit of the 80s!');
+INSERT INTO `cs453db2`.`site_products` (`Product_ID`, `UPCCode`, `ProductName`, `ProductSize`, `ProductWeight`,`ProductDescription`) VALUES (9, 98332, 'The Breakfast Club', 'small', '12g','When a group of misguided teens from different walks of life  attend saturday school, they find a lot out about their classmates, and even more baout themselves.');
+INSERT INTO `cs453db2`.`site_products` (`Product_ID`, `UPCCode`, `ProductName`, `ProductSize`, `ProductWeight`,`ProductDescription`) VALUES (10, 98333, 'Smokey and the Bandit', 'small', '12g','No matter how many smokies are on his trail the bandit is east bound and down, loaded up and truckin.');
+
+#games
+INSERT INTO `cs453db2`.`site_products` (`Product_ID`, `UPCCode`, `ProductName`, `ProductSize`, `ProductWeight`,`ProductDescription`) VALUES (11, 98334, 'Super Mario Bros.', 'small', '12g','The NES classic!');
+INSERT INTO `cs453db2`.`site_products` (`Product_ID`, `UPCCode`, `ProductName`, `ProductSize`, `ProductWeight`,`ProductDescription`) VALUES (12, 98335, 'Sonic', 'small', '12g','Who knew hedgehogs could move so fast?!');
+INSERT INTO `cs453db2`.`site_products` (`Product_ID`, `UPCCode`, `ProductName`, `ProductSize`, `ProductWeight`,`ProductDescription`) VALUES (13, 98336, 'Kirby', 'small', '12g','Watch him eat everything!');
+INSERT INTO `cs453db2`.`site_products` (`Product_ID`, `UPCCode`, `ProductName`, `ProductSize`, `ProductWeight`,`ProductDescription`) VALUES (14, 98337, 'Frogger', 'small', '12g','Help Frogger, the frog, get his friends back!');
+INSERT INTO `cs453db2`.`site_products` (`Product_ID`, `UPCCode`, `ProductName`, `ProductSize`, `ProductWeight`,`ProductDescription`) VALUES (15, 98338, 'Silent Hill 2', 'small', '12g','Make sure to play this with the lights off.');
